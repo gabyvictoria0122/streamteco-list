cf577bf980b067fddc5fd1038b42547198ccf0ed
Gabriela Victoria <gabrielavictoria91@gmail.com>
2023-03-23 18:21:27 -0300

    renderiza obj

--- frontend/src/components/Search.vue
+++ frontend/src/components/Search.vue
@@ -10,12 +10,39 @@
         :loading="userloading"
         :search-input.sync="moviessearch"
         item-text="login"
+        @keyup.enter="searchMovies"
       />
 </template>
 
 <script>
+import { useAppStore } from "@/stores/appStore"
+import TasksApi from "@/api/tasks.api.js"
+
+
 export default {
   name: "SearchMovies",
+  setup() {
+    const appStore = useAppStore()
+    return { appStore }
+  },
+  data() {
+    return {
+      loading: false,
+      moviesearch: [],
+    }
+  },
+  mounted() {
+    this.getTasks()
+  },
+  methods: {
+    getTasks() {
+      this.loading = true
+      TasksApi.getTasks().then((data) => {
+        this.moviesearch = data.todos
+        this.loading = false
+      })
+    },
+  },
 }
 </script>
 
